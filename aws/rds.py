0d4e9b2878a3919f914e75e0f69eabd416c45fae
Ayush Nema <94337759+Ayush-Nema@users.noreply.github.com>
2024-01-17 15:19:21 +0530

    Update rds.py

--- aws/rds.py
+++ aws/rds.py
@@ -3,49 +3,9 @@ import sys
 
 import pandas as pd
 import psycopg2
-from sqlalchemy import URL, create_engine
-from sqlalchemy.orm import sessionmaker
 
 # Initialising the logger
 LOGGER = logging.getLogger(__name__)
-
-
-def connect_to_rds_with_sqlalchemy(rds_host: str, rds_port: str, rds_username: str, rds_user_pwd: str,
-                                   rds_db_name: str):
-    """
-    Connecting to RDS with SQLAlchemy. This creates a connection engine.
-    :param rds_host:
-    :param rds_port:
-    :param rds_username:
-    :param rds_user_pwd:
-    :param rds_db_name:
-    :return:
-    """
-
-    try:
-        url_object = URL.create(
-            "postgresql+psycopg2",
-            username=rds_username,
-            password=rds_user_pwd,  # plain (unescaped) text
-            host=rds_host,
-            database=rds_db_name,
-            port=int(rds_port)
-        )
-        engine = create_engine(url_object, echo=False)
-
-        # Create connection to DB using engine
-        conn = engine.connect()
-
-        # Create session
-        session = sessionmaker(bind=engine)
-        session = session()
-
-        return conn, engine, session
-
-    except Exception as e:
-        LOGGER.error(f"ERROR: Could not connect to Postgres instance. \n {e}",
-                     exc_info=True, stack_info=True)
-        sys.exit(1)
 
 
 def connect_to_rds(rds_host: str, rds_port: str, rds_username: str, rds_user_pwd: str, rds_db_name: str):
