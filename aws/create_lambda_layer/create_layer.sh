8714a1a4ee4ce6474454461dba0b4a4799380a02
Ayush Nema <94337759+Ayush-Nema@users.noreply.github.com>
2024-04-17 17:30:41 +0530

    Create create_layer.sh

---
+++ aws/create_lambda_layer/create_layer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set the directory where the Dockerfile and requirements.txt are located
+DIRECTORY="$(pwd)"
+
+# Change it as per your requirement
+LAYER_NAME="sqlalchemy-layer"
+
+# Build the Docker image
+docker build -t lambda-layer "$DIRECTORY"
+
+# Run the Docker container to create the layer
+docker run --name lambda-layer-container -v "$DIRECTORY:/app" lambda-layer
+
+# create layers directory, if not created.
+mkdir -p layers
+
+# Move the zip file in layers directory.
+mv "$DIRECTORY/$LAYER_NAME.zip" "$DIRECTORY/layers/$LAYER_NAME.zip"
+
+# Stop the container
+docker stop lambda-layer-container
+
+# Remove the running container
+docker rm lambda-layer-container
+
+# Cleanup: remove the Docker image
+docker rmi --force lambda-layer
