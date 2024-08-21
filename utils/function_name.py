dc1e35b8e158a249a83b1d5324336d33f6f67b41
Ayush Nema <94337759+Ayush-Nema@users.noreply.github.com>
2024-08-21 14:44:19 +0530

    Update function_name.py

--- utils/function_name.py
+++ utils/function_name.py
@@ -7,11 +7,11 @@ import inspect
 
 
 def utility_func(a, b):
-    print(f"Triggered [{inspect.currentframe().f_code.co_name}] with i/p args: {locals()}")
+    print(f"Triggered [`{inspect.currentframe().f_code.co_name}`] with i/p args: {locals()}")
     return a + b
 
 
 if __name__ == '__main__':
     _ = utility_func(1, 2)
-    # >> Triggered [utility_func] with i/p args: {'a': 1, 'b': 2}
+    # >> Triggered [`utility_func`] with i/p args: {'a': 1, 'b': 2}
 
